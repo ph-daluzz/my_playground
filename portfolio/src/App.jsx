8aaac77a73abaeab760d0d6f2db75aa854ba98fc
ph-daluzz <ctt-pedrohl@gmail.com>
2025-08-19 21:33:10 -0300

    ajustes no design

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import { Target, User, Leaf } from "lucide-react";
 
 function App() {
   return (
@@ -25,15 +26,24 @@ function App() {
           </div>
 
           <div id='container-interesses'>
-            <div id="interesse-profissional">
-              <p>Meu objetivo profissional</p>
+
+            <div className="interesse-profissional">
+              <p className='interesse-profissional-titulo'><Target size={20} className="icon" /> Meu objetivo profissional</p>
+              <p className='interesse-profissional-descricao'>Meu objetivo profissional é atuar no desenvolvimento de software, com foco em aplicações mobile, sempre evoluindo tecnicamente e colaborando em projetos desafiadores. Também estou aberto a oportunidades em front e back-end.</p>
             </div>
-            <div id="interesse-profissional">
-              <p>Meu objetivo profissional</p>
+            <div className="sobre-mim">
+              <p className='sobre-mim-titulo'> <User size={20} className="icon" />Sobre mim</p>
+              <p className='sobre-mim-descricao'>Sou apaixonado por tecnologia desde criança, sempre curioso sobre como as coisas funcionam por trás das telas. Essa curiosidade me levou a estudar Desenvolvimento de Software no SENAI de São José, onde tenho evoluído como profissional e sigo em busca do meu sonho de trabalhar com tecnologia.
+              </p>
+              <p className='sobre-mim-descricao'>Atualmente atuo em uma fábrica de máquinas de reciclagem como operador de máquina e programador CNC, o que me deu experiência prática em ambientes de produção e de trabalho em equipe.</p>
             </div>
-            <div id="interesse-profissional">
-              <p>Meu objetivo pessoal</p>
+
+            <div className="interesse-pessoal">
+              <p className='interesse-pessoal-titulo'><Leaf size={20} className="icon" />Meu objetivo pessoal</p>
+              <p className='interesse-pessoal-descricao'>Meu objetivo pessoal é manter equilíbrio entre vida profissional e pessoal, dedicando tempo a família, amigos e hobbies. Gosto de aprender continuamente, seja em cursos, leituras ou experiências práticas, porque acredito que crescer como pessoa me ajuda a evoluir também como profissional.</p>
             </div>
+
+
           </div>
         </div>
 
