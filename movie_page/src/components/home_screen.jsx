7d9709ab1aa9e49169e5f2f09b09d2a4c615161f
ph-daluzz <ctt.pedrohl@gmail.com>
2025-09-19 01:21:03 -0300

    Implementado  design de subcomponente 'renderizarFilmes'

--- movie_page/src/components/home_screen.jsx
+++ movie_page/src/components/home_screen.jsx
@@ -8,6 +8,9 @@ export function HomeScreen() {
 
 
   const handleMovieSearch = (event) => {
+    if (!filmePesquisa.trim())
+      return
+
     axios.get(`${process.env.REACT_APP_API_URL}&s=${filmePesquisa}`)
       .then(response => {
         // coloca os filmes encontrados na variável de estado
@@ -24,22 +27,28 @@ export function HomeScreen() {
   return (
     <div style={styles.container}>
 
-      <header style={stylesHeader.headerContainer}>
+      <header style={stylesHeader.header_container}>
         <h1 style={stylesHeader.h1}>
           AbsoluteCinema
         </h1>
-        <input type="text"
-          style={stylesHeader.input}
-          placeholder="Procurar filme"
-          onChange={(event) => setFilmePesquisa(event.target.value)} />
+        <div>
 
-        <button onClick={handleMovieSearch}>Buscar</button>
+          <input type="text"
+            style={stylesHeader.input}
+            placeholder="Procurar filme"
+            onChange={(event) => setFilmePesquisa(event.target.value)} />
+          <button onClick={handleMovieSearch} style={stylesHeader.input_button}>Buscar</button>
+        </div>
       </header>
 
-      <main style={styles.mainStyle}>
-        <h2>Conteúdo principal</h2>
-        {/* Componente de renderização dos filmes encontrados */}
-        <RenderizarFilmes filmesEncontrados={filmeEncontrado} />
+      <main style={styles.main_style}>
+        <section style={styles.main_section}>
+          <section style={styles.main_section_sidebar}>
+            <p>Selecione um filtro</p>
+          </section>
+          {/* Componente de renderização dos filmes encontrados */}
+          <RenderizarFilmes filmesEncontrados={filmeEncontrado} />
+        </section>
       </main>
     </div>
   );
@@ -48,34 +57,72 @@ export function HomeScreen() {
 // CSS in JS to container
 const styles = {
   container: {
-    height: '100vh',
+    height: '100%',
     width: '100%',
     display: 'flex',
     flexDirection: 'column',
-    alignItems: 'center',
   },
+
+  main_style: {
+    padding: '10px',
+    width: '100%',
+    backgroundColor: 'lightgray'
+  },
+
+  main_section: {
+    display: 'flex',
+    justifyContent: 'start',
+
+  },
+
+  main_section_sidebar: {
+    backgroundColor: 'white',
+
+    display: 'flex',
+    justifyContent: 'center',
+
+    width: '250px',
+    maxHeight: '70vh',
+
+    boxShadow: '8px 8px 10px rgba(0, 0, 0, 0.2)',
+
+    padding: '10px',
+    margin: '0px 15px 10px 0px',
+    borderRadius: '5px'
+
+  }
 };
 
 
 // CSS in JS to header
 const stylesHeader = {
-  headerContainer: {
+
+  header_container: {
     width: '100%',
-    padding: '1vh',
+    height: 'fit-content',
     backgroundColor: '#530000ff',
     display: 'flex',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
     alignItems: 'center',
   },
+
   input: {
     marginLeft: '2vw',
-    height: '5vh',
-    width: '20vw',
+    height: '2vh',
+    width: '60vw',
     padding: '0.5vh',
     borderRadius: '4px',
-    border: '1px solid #ccc',
+    border: '1px solid #ffffffff',
   },
   h1: {
     color: 'red',
+  },
+  input_button: {
+    height: '3vh',
+    width: '7vw',
+
+    border: 'none',
+    borderRight: '2px white solid'
+
   }
 };
--- movie_page/src/components/subcomponents/renderizarFilmes.jsx
+++ movie_page/src/components/subcomponents/renderizarFilmes.jsx
@@ -1,5 +1,4 @@
 export function RenderizarFilmes({ filmesEncontrados }) {
-
     // filtrar filmes para não haver renderização duplicada nos IDs
     const filmesUnicos = filmesEncontrados.filter(
         (filme, index, self) =>
@@ -7,29 +6,71 @@ export function RenderizarFilmes({ filmesEncontrados }) {
     );
 
     return (
-        <div style={styles.containerFilmes}>
-            {filmesUnicos.map(filme =>
-                <div key={filme.imdbID} style={styles.filmeCard}>
-                    <h3>
-                        <strong>
-                            {filme.Title}
-                        </strong>
-                    </h3>
-                    <p>
-                        <strong>
-                            {filme.Year}
-                        </strong>
-                    </p>
-                    <img src={filme.Poster} alt={filme.Title} />
+        <div style={styles.container_filmes}>
+            {filmesUnicos.map(filme => (
+                <div key={filme.imdbID} style={styles.card_filme}>
+                    <img src={filme.Poster} alt={filme.Title} style={styles.poster} />
+                    <h3 style={styles.titulo_filme}>{filme.Title}</h3>
+                    <p>{filme.Year}</p>
+                    <button style={styles.botao_assistir}><strong>VER TRAILER</strong></button>
                 </div>
-            )
-            }
-        </div >
-    )
+            ))}
+        </div>
+    );
 }
 
 const styles = {
+  container_filmes: {
+    display: 'grid',
+    gap: '10px',
+    gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))',
+    width: '100%',
+    padding: '20px',
+    boxSizing: 'border-box',
+  },
 
+  card_filme: {
+    backgroundColor: 'white',
+    padding: '5px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent:'space-around',
+    borderRadius: '10px',
+    boxShadow: '0 2px 8px rgba(0,0,0,0.1)',
+  },
 
+  poster: {
+    width: '100%',
+    height: '300px',
+    borderRadius: '5px',
+    objectFit: 'cover',
+    marginBottom: '5px',
+  },
 
-}
+  titulo_filme: {
+    textAlign: 'center',
+    margin: '10px 0 4px 0',
+    fontSize: '1.1rem',
+  },
+
+  ano_filme: {
+    margin: 0,
+    fontSize: '0.9rem',
+    color: 'gray',
+  },
+
+  botao_assistir: {
+    cursor: 'pointer',
+    width: '100%',
+    padding: '10px',
+    border: 'none',
+    borderTop: '1px solid #ccc',
+    backgroundColor: '#f44336',
+    color: 'white',
+    fontWeight: 'bold',
+    borderRadius: '0 0 10px 10px',
+    transition: 'background-color 0.3s',
+  },
+};
+;
