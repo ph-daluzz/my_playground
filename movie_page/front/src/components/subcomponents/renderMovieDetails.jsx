8c8a01ef3882a4f88552b91a11aeac80ac231409
ph-daluzz <ctt.pedrohl@gmail.com>
2025-09-30 23:52:48 -0300

    criado componente

--- movie_page/front/src/components/subcomponents/renderMovieDetails.jsx
+++ movie_page/front/src/components/subcomponents/renderMovieDetails.jsx
@@ -1,14 +1,42 @@
+import axios from 'axios';
+import { useEffect, useState } from "react"
+import { Similar } from './renderSimilarMovies';
+
 export function Details({ movie }) {
+    const [alternativeMovies, setAlternativeMovies] = useState([])
+    // buscar filmes recomendados 
+    useEffect(() => {
+        if (!movie) {
+            return
+        }
+        const filmesAlternativos = async () => {
+            try {
+                const filmes = await axios.get(`http://localhost:3000/filmes_alternativos/${movie.id}`)
+                console.log(filmes.data)
+                setAlternativeMovies(filmes.data)
+            } catch (e) {
+                console.log(e, "erro no front, filmes alternativos")
+            }
+        }
+        filmesAlternativos()
+    }, [movie])
+
     if (!movie) return <p>Carregando...</p>
     return (
         <div style={{
-            ...styles.background.base,
-            backgroundImage: `
-          linear-gradient(to top, rgba(0,0,0,0.7), rgba(0,0,0,0.2)),
-          url(${movie.backdropURL})
-        `
+            ...styles.background.base
         }}>
-            <h1>{movie.title}</h1>
+            <div style={styles.container_actions}>
+                <img src={movie.posterURL} alt="cover filme" style={styles.imagePoster} />
+                <div style={styles.movie_description}>
+                    <h1>{movie.title}</h1>
+                    <p>{movie.overview}</p>
+                    <div>
+                        <button style={styles.watch_trailer_button}>Ver trailer</button>
+                    </div>
+                </div>
+            </div>
+            <Similar movie_list={alternativeMovies} />
         </div>
     )
 
@@ -18,22 +46,62 @@ const styles = {
     background: {
         base: {
             width: '100%',
-            height: '100vh',
+
             backgroundSize: 'cover',
             backgroundPosition: 'center',
+            backgroundAttachment: 'fixed',
+
             display: 'flex',
             flexDirection: 'column',
-            justifyContent: 'flex-end',
-            padding: '40px',
-            color: 'white'
+            paddingTop: '60px',
+            paddingLeft: '20px',
+            paddingRight: '20px',
+            boxSizing: 'border-box',
+            color: 'white',
         }
     },
+
+    container_actions: {
+        display: 'flex',
+        alignItems: "center",
+        justifyContent: "center",
+
+        width: 'fit-content',
+        minWidth: '70vh',
+
+        padding: '10px',
+        border: "2px solid rgba(247, 247, 247, 0.2)",
+        borderRadius: '10px',
+
+        backgroundColor: "rgba(0, 0, 0, 0.5)",
+        marginBottom: '50px'
+    },
+
+    movie_description: {
+        width: '70vh',
+        height: '100%',
+
+        padding: '10px',
+
+        display: 'flex',
+        alignItems: "center",
+        flexDirection: "column",
+
+    },
+    imagePoster: {
+        width: '350px',
+        height: 'auto',
+        borderRadius: '10px'
+    },
+
     title: {
         fontSize: '3rem',
         margin: 0
     },
-    overview: {
-        fontSize: '1.2rem',
-        maxWidth: '600px'
+
+    watch_trailer_button: {
+        height: '5vh',
+        width: '15vh'
     }
+
 }
