bf054ab0a9fe223ce0f4a1d1ad18576d5dd0cfc6
ph-daluzz <ctt.pedrohl@gmail.com>
2025-09-30 23:48:04 -0300

    adicionado rota e endpoint para funcao de resgatar filmes similares ao selecionado

--- movie_page/back/routes/rotasFilmes.js
+++ movie_page/back/routes/rotasFilmes.js
@@ -3,11 +3,13 @@ const { Router } = require('express')
 const { buscarFilmes } = require('../controllers/movieSearch.js')
 const { trendingMovies } = require('../controllers/searchTopMovies.js')
 const { detalhes } = require('../controllers/getMovieDetails.js')
+const { getAlternativeMovies } = require('../controllers/searchAlternativeTitles.js')
 
 const router = Router()
 
 router.get('/search/:movieName', buscarFilmes)
 router.get('/topMovies', trendingMovies)
 router.get('/detalhes/:id', detalhes)
+router.get('/filmes_alternativos/:id', getAlternativeMovies)
 
 module.exports = router
